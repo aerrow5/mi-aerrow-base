1cc720d77a7e0b4d0468b217b746056889b994c0
Arthur van der Peijl <aavanderpeijl@gmail.com>
2016-01-30 20:36:16 +0100

    Update 00-core-smf.sh

--- copy/var/zoneinit/includes/00-core-smf.sh
+++ copy/var/zoneinit/includes/00-core-smf.sh
@@ -1,7 +1,8 @@
-# This script load all SMF manifests from /opt/core/lib/svc/manifest
+# This script load all SMF manifests from /opt/local/lib/svc/manifest
 
-CORE_SVC_DIR='/opt/core/lib/svc/manifest/'
+CORE_SVC_DIR='/opt/local/lib/svc/manifest/'
 
-for xml in ${CORE_SVC_DIR}*; do
-	svccfg import ${xml}
-done
+# temporary disables
+#for xml in ${CORE_SVC_DIR}*; do
+#	svccfg import ${xml}
+#done
